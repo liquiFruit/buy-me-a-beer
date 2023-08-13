5cfff2e6ae32ec189c5165b282d6f8eead9c6b34
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-08-14 01:54:01 +0200

    add skeleton next-auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,27 @@
+import type { NextAuthOptions, User } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "username"
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password"
+        }
+      },
+      async authorize(credentials) {
+        const user: User = { id: "0", name: "testUsername", email: "testEmail", password: "test" }
+        return user
+      }
+    })
+  ]
+}
