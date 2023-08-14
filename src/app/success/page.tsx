0e20c13343133f9657fe59e334f90cc9148b2201
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-08-15 00:54:15 +0200

    touch up success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,3 +1,49 @@
-export default function SuccessfullPayment() {
-	return <div>Successfull Payment</div>
+import { getServerSession } from "next-auth"
+import { options } from "../api/auth/[...nextauth]/options"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+	CardContent,
+	CardFooter,
+} from "@/components/ui/card"
+
+export default async function SuccessfullPayment() {
+	const session = await getServerSession(options)
+	if (!session || !session.user || !session.user.email)
+		return <div>error: not authenticated</div>
+
+	return (
+		<main className="w-full h-screen grid place-items-center">
+			<Card>
+				<CardHeader>
+					<CardTitle>
+						Thanks,{" "}
+						<span className="text-primary">
+							{session.user.name}
+						</span>
+						!
+					</CardTitle>
+					<CardDescription>
+						Payment was completed successfully.
+					</CardDescription>
+				</CardHeader>
+
+				<CardContent>
+					We thank you for your donation, and pray that you don't want
+					it back.
+				</CardContent>
+
+				<CardFooter>
+					<Button asChild className="w-full">
+						<Link href={"/"}>Go to home</Link>
+					</Button>
+				</CardFooter>
+			</Card>
+		</main>
+	)
 }
